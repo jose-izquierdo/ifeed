df829f95abd79a2f8fed934ae21c2fe32c9e9aca
Jose A. Izquierdo <jainguns@gmail.com>
2014-03-25 12:23:37 +0000

    First commit of iFeed

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ifeed::Application.config.secret_key_base = '7cda87412decadfffd217c162e512ef765cd3a544f885223bca7b06048452397adc10383fe48d19d50cacd69874201d5cdf6ac30b255e3d13c6676aa7c1ee184'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ifeed::Application.config.session_store :cookie_store, key: '_ifeed_session'
