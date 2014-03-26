2761d9e268ffff41aecbe09303571684cd2b2bb7
Jose A. Izquierdo <jainguns@gmail.com>
2014-03-26 12:21:50 +0000

    Tech feeds created on the controller

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -2,11 +2,21 @@ class FeedsController < ApplicationController
 	def index
 	end
 	def create
+
+		#Creating tech feeds from NYT, LAT & 
+		urls = %w[http://rss.nytimes.com/services/xml/rss/nyt/Technology.xml http://feeds.feedburner.com/latimes/technology http://feeds.bbci.co.uk/news/technology/rss.xml?edition=uk]
+		tech_feeds = Feedjira::Feed.fetch_raw urls
 	end
+
 	def edit
 	end
+
+	def show
+	end
+
 	def update
 	end
+
 	def delete
 	end
 end
